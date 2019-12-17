902926f5d86d48eb7df0077ef29fcab7a7830e55
wolfie <www.test.com>
2019-12-17 17:00:13 +0800

    加入模块化css样式

--- src/component/Hello.jsx
+++ src/component/Hello.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+
+import helloCss from '@/css/hello.css'
+
 export default class Hello extends React.Component{
     constructor(props) {
         super(props);
     }
     render(){
-        return <div><h3>这是一个hello组件---{this.props.name}</h3></div>
+        return <div><h3 className={helloCss.title1}>这是一个hello组件---{this.props.name}</h3></div>
 
     }
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,24 @@ module.exports = {
     // mode:'production'
     , module: {
         rules: [
-            {test:/\.js|jsx$/,use:['babel-loader'], exclude: /node_modules/}
+            {test:/\.js|jsx$/,use:['babel-loader'], exclude: /node_modules/},
             //这里exclude后面的值是一个js的正则对象，不是字符串，所以没有引号，所以一定不要以为是字符串的路径
+            {
+             test:/\.css/,
+             use:[
+                    {
+                        loader:'style-loader'
+                    },
+                    {
+                        loader:'css-loader',
+                        options:{
+                            modules:{
+                                localIdentName:'[path][name]-[local]-[hash:5]'
+                            }
+                        }
+                    }
+             ],
+            exclude: /node_modules/}
         ]
     },
     resolve: {
